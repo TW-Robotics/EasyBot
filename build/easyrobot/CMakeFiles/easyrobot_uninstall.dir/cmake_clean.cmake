30033b3c6c0dd4b938c1949590c6385cbf8f2e6b
Michael Schebek <103026337+michaelschebek@users.noreply.github.com>
2025-03-14 14:49:26 +0100

    Delete build directory

--- build/easyrobot/CMakeFiles/easyrobot_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/easyrobot_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/easyrobot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
